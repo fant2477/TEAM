d4f8bc2a9819eac9e21262726488b189d88477ed
Araya Siriadun <plaitiang@gmail.com>
2017-06-14 10:52:00 +0700

    something

--- RunningDocument.java
+++ RunningDocument.java
@@ -14,6 +14,9 @@ public class RunningDocument {
 	String currentUser;
 
 	public RunningDocument(String currentUser) {
+		if (!AccountValidaiton.isUsenameTaken(currentUser)) {
+			System.err.println("This username doesn't exist.");
+		}
 		this.currentUser = currentUser;
 	}
 
@@ -74,30 +77,32 @@ public class RunningDocument {
 			e1.printStackTrace();
 		}
 		if (f.exists()) {
+			int currentID = getLastID() + 1;
 			ConnectionDB.connect();
 			try {
-				String currentTime = RunningDocument.getFullCurrentTime();
-				String sql = String
-						.format("INSERT INTO Document VALUES(%d, '%s', '%s','%s', '%s', '%s', '%s')",
-								getLastID() + 1, f.getName(), currentTime,
-								RunningDocument.toSize(f.length()),
-								this.currentUser, FilePath, detail);
-				ConnectionDB.statement.executeUpdate(sql);
-				System.out.println(String.format("Added %s Correctly.",
-						f.getName()));
-
 				// Add Data of file to DataBase.
-				sql = ("INSERT INTO DataFile VALUES(?,?,?,?)");
+				String sql = ("INSERT INTO DataFile VALUES(?,?,?,?)");
 				pstmt = ConnectionDB.connect.prepareStatement(sql);
-				pstmt.setInt(1, getLastID() + 1);
+				pstmt.setInt(1, currentID);
 				pstmt.setString(2, f.getName());
 				pstmt.setDouble(3, f.length());
 				pstmt.setBinaryStream(4, fis, f.length());
 				pstmt.executeUpdate();
+				String currentTime = RunningDocument.getFullCurrentTime();
 				pstmt.close();
+				System.out.println(String.format("Added %s Correctly.",
+						f.getName()));
+
+				// Add file to Document
+				sql = String
+						.format("INSERT INTO Document VALUES(%d, '%s', '%s','%s', '%s', '%s', '%s')",
+								currentID, f.getName(), currentTime,
+								RunningDocument.toSize(f.length()),
+								this.currentUser, FilePath, detail);
+				ConnectionDB.statement.executeUpdate(sql);
 
 				// Add log
-				this.addLog("added", f.getName(), getLastID() + 1, FilePath,
+				this.addLog("added", f.getName(), currentID, FilePath,
 						currentTime);
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -109,13 +114,14 @@ public class RunningDocument {
 	}
 
 	public void deleteFile(int id) {
+		String name = "";
+		String path = "";
 		ConnectionDB.connect();
 		try {
+			// get filename and path
 			String sql = String.format("SELECT * FROM Document WHERE ID = %d",
 					id);
 			ResultSet rs = ConnectionDB.statement.executeQuery(sql);
-			String name = "";
-			String path = "";
 			if (rs.next()) {
 				name = rs.getString("Filename");
 				path = rs.getString("Path");
@@ -139,14 +145,14 @@ public class RunningDocument {
 		ConnectionDB.disconnect();
 	}
 
-	public void getFile(int id, String targetPath) {
+	public static void getFile(int id, String targetPath) {
 		byte[] fileBytes;
 		String query;
 		try {
 			ConnectionDB.connect();
 			query = String.format("SELECT * FROM DataFile WHERE ID = %d", id);
-			Statement state = ConnectionDB.connect.createStatement();
-			ResultSet rs = state.executeQuery(query);
+			// Statement state = ConnectionDB.connect.createStatement();
+			ResultSet rs = ConnectionDB.statement.executeQuery(query);
 			if (rs.next()) {
 				fileBytes = rs.getBytes("Data");
 				String name = rs.getString("Filename");
@@ -179,6 +185,7 @@ public class RunningDocument {
 		ConnectionDB.disconnect();
 	}
 
+	// Unused
 	public static int getMaxLenFilename() {
 		int maxLen = 0;
 		int currentLen;
@@ -198,32 +205,6 @@ public class RunningDocument {
 		}
 		ConnectionDB.disconnect();
 		return maxLen;
-	}
-
-	public void getAllHistory() {
-		ConnectionDB.connect();
-		try {
-			String sql = String.format("SELECT * FROM History ORDER BY Time");
-			ResultSet rs = ConnectionDB.statement.executeQuery(sql);
-			while (rs.next()) {
-				int id = rs.getInt("ID");
-				String descript = rs.getString("Event");
-				Timestamp ts = rs.getTimestamp("Time");
-
-				// Display values
-
-				System.out.println("Time: "
-						+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
-								.format(ts));
-				System.out.print(": " + descript);
-				System.out.println(" ID: " + id);
-
-			}
-			rs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		ConnectionDB.disconnect();
 	}
 
 	public void changePassword(String oldPass, String newPass) {
