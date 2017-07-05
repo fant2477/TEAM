7bd6beb0f472617dda39357fb2a94833af8fdf52
fant2477 <fant_2477@hotmail.com>
2017-07-05 11:14:12 +0700

    before add delete fn

---
+++ fant/5-7-60/WebContent/header_ui.jsp
@@ -0,0 +1,245 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+<%@ page import="java.util.*"%>
+<%@ page import = "java.io.*"%>
+<%@ page import= "project_db.*" %>
+
+<html>
+<head>
+<title>Running Document</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="./header_ui_css.css">
+
+
+
+
+</head>
+<body>
+
+
+<form id="detail" method="post" action="header_page">
+
+<!-- ============================================ start of header tab ============================================ -->
+
+     <nav class="navbar navbar-inverse navbar-static-top">
+
+           <div class="container-fluid">
+
+
+
+                 <!-- Brand and toggle get grouped for better mobile display -->
+                 <div class="navbar-header">
+
+                       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+
+                           <span class="sr-only">Toggle navigation</span>
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+
+                       </button>
+
+                       <h4 class="navbar-text">Running Document</h4>
+                 </div>
+
+
+
+
+                 <!-- Collect the nav links, forms, and other content for toggling -->
+                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+
+
+                       <ul class="nav navbar-nav">
+
+                             <li><p><input type="submit" name="bt" value="Main Page" class="btn-link" id = "submit"></p></li>
+
+                             <li><p><input type="submit" name="bt" value="History Page" class="btn-link" id = "submit"></p></li>
+
+                             <!-- <li class="active"><a href="#">Add Page <span class="sr-only">(current)</span></a></li> -->
+                             <li><p><input type="submit" name="bt" value="Add Page" class="btn-link" id = "submit"></p></li>
+
+                             <li><p><input type="submit" name="bt" value="Delete Page" class="btn-link" id = "submit"></p></li>
+
+
+
+                       </ul>
+
+
+
+                       <ul class="nav navbar-nav navbar-right">
+                              <!-- <a class="navbar-brand" href="#">
+                               <img alt="User" src="./login_img.png">
+                             </a>  -->
+
+                             <li><p><input type="submit" name="bt" value="Log Out" class="btn-link" id = "submit"></p></li>
+                       </ul>
+                       <ul class="nav navbar-nav navbar-right">
+                          <button type="submit" class="btn-link" name="bt" value="User_info" id="User_info"><i class="fa fa-user-circle-o"></i> </button>
+
+
+                       </ul>
+
+                 </div>
+
+           </div><!-- /.container-fluid -->
+
+     </nav>
+</br>
+<!-- ============================================ end of header tab ============================================ -->
+</br> <h1>Document Header Details</h1> </br></br>
+<div class="container">
+
+
+		<div class="form-inline">
+                  <!-- <p align = "center"> -->
+                  <%-- <h3>Document Details</h3>
+                  <hr size="4" color="gray"/> --%>
+
+<%-- ========================search group======================== --%>
+                  <div class="input-group">
+                      <input type="text" id="search_input" class="form-control" placeholder="Search for..." aria-label="Search for...">
+                      <div class="input-group-btn">
+                        <!-- Buttons -->
+                          <button id="search_info" class="btn btn-default" type="button"><i class="glyphicon glyphicon-info-sign" style="font-size:20px" onclick="alert('Please enter only Filename or Document ID.');"></i></button>
+                          <button type="submit" name="bt" class="btn btn-default" id="search_bt" value="search_bt"><i class="glyphicon glyphicon-search" style="font-size:20px" ></i></button>
+                          <%-- <button type="submit" class="btn-link" name="bt" value="User_info" id="User_info"><i class="fa fa-user-circle-o"></i> </button> --%>
+
+                          <%-- <button id="search_bt" class="btn btn-default" type="button">Go!</button> --%>
+                      </div>
+                  </div>
+			</div>
+<%-- ======================== start of table ======================== --%>
+</br>
+                <%-- <div class="container2"> --%>
+                  <div class="row">
+                      <table class="table table-striped table-bordered" >
+                        		<tr>
+                        			<th>Num</th>
+                        			<th>ID</th>
+                        			<th>Doc Name</th>
+                        			<th>Created by</th>
+                        			<th>Last Modified</th>
+                        		</tr>
+
+                        	<c:forEach items="${doclist}" var="doc" varStatus="pstatus">
+                        		<tr>
+
+                        			<%UserManager um = new UserManager();
+                        			Time ti = new Time();%>
+                        			<c:set var="doc1" value="${doc}"></c:set>
+
+                        			<td>${(pstatus.index)+1}</td>
+
+                        			<td><p><input type="submit" name="bt" value="${doc.getDoc_ID()}" class="link_detail" id = "submit"></p></td>
+                        			<td>${doc.getDoc_name()}</td>
+
+                        			<td><% out.print(um.getUsername( ( (DocumentDetail)pageContext.getAttribute("doc1") ).getUser_ID_created()) );%></td>
+                        			<td><% out.print(ti.datetoReadableString( ( (DocumentDetail)pageContext.getAttribute("doc1") ).getDate_modified()) );%></td>
+
+
+
+                        		</tr>
+                        	</c:forEach>
+
+                        </table>
+
+
+                        </br>
+
+                  </div>
+                <%-- </div> --%>
+<%-- ======================== end of table ======================== --%>
+
+
+
+        <div class="box form-group">
+            <input type="file"  name="file-6[]" id="file-6" class="inputfile inputfile-5" data-multiple-caption="{count} files selected" multiple />
+            <label for="file-6"><figure><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg></figure> <span></span></label>
+        </div>
+
+
+        <input type="submit" class="btn btn-default btn-lg bt form-group" name = "bt"  value="Add files" id = "addfile" />
+        <input type="submit" class="btn btn-default btn-lg bt form-group" name = "bt"  value="Delete Document" id = "delete_doc" />
+
+
+<!-- ===============start of script for file button=============== -->
+       <script>
+           var inputs = document.querySelectorAll( '.inputfile' );
+           Array.prototype.forEach.call( inputs, function( input )
+           {
+             var label	 = input.nextElementSibling,
+               labelVal = label.innerHTML;
+
+             input.addEventListener( 'change', function( e )
+             {
+               var fileName = '';
+               if( this.files && this.files.length > 1 )
+                 fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+               else
+                 fileName = e.target.value.split( '\\' ).pop();
+
+               if( fileName )
+                 label.querySelector( 'span' ).innerHTML = fileName;
+               else
+                 label.innerHTML = labelVal;
+             });
+           });
+       </script>
+<!-- ===============end of script for file button=============== -->
+
+<%-- ========================start of paging======================== --%>
+                  <nav  aria-label="Page navigation">
+                      <div class="text-center">
+                          <ul class="pagination pagination-sm ">
+
+                              <li>
+                                  <a href="#" class ="paging disabled" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>
+                              </li>
+
+                              <li><a href="#" class ="paging"> < </a></li>
+
+                              <li><a href="#" class ="paging">1</a></li>
+
+                              <li class="active"><a href="#" class ="paging">2<span class="sr-only">(current)</span></a></li>
+
+                              <li><a href="#" class ="paging">3</a></li>
+
+                              <%-- <li><a href="#" class ="paging">4</a></li>
+
+                              <li><a href="#" class ="paging">5</a></li> --%>
+
+                              <li><a href="#" class ="paging"> > </a></li>
+
+                              <li>
+                                  <a href="#" class ="paging" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>
+                              </li>
+
+                          </ul>
+                      </div>
+                  </nav>
+
+
+
+<%-- ========================end of paging======================== --%>
+
+
+
+</div>
+
+
+</form>
+
+<!-- ===============start of script for file button=============== -->
+
+<!-- ===============end of script for file button=============== -->
+
+</body>
+<!--jsp:include page = "footer.jsp"/-->
+
+</html>
---
+++ fant/5-7-60/src/project_db/DocumentManager.java
@@ -0,0 +1,311 @@
+package project_db;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Date;
+
+public class DocumentManager {
+    private static User currentUser = null;
+    private static DocumentHeader currentHeader = null;
+
+    public DocumentManager(User currentUser) {
+        DocumentManager.currentUser = currentUser;
+    }
+
+    public static DocumentDetail getFile(int id) {
+        DocumentDetail d = null;
+        try {
+            String sql = String.format("SELECT * FROM Document_detail WHERE Doc_ID = %d", id);
+            ResultSet rs = ConnectionDB.statement.executeQuery(sql);
+            if (rs.next()) {
+                d =
+                        new DocumentDetail(
+                                rs.getInt("Doc_ID"),
+                                rs.getInt("Doc_header_ID"),
+                                rs.getString("Doc_name"),
+                                rs.getTimestamp("Date_created"),
+                                rs.getTimestamp("Date_modified"),
+                                rs.getInt("User_ID_created"),
+                                rs.getInt("User_ID_modified"),
+                                rs.getLong("Size"),
+                                rs.getBytes("Data_file"));
+                rs.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return d;
+    }
+
+    public static DocumentDetail getGenneralFile(int id) {
+        DocumentDetail d = null;
+        try {
+            String sql =
+                    String.format(
+                            "SELECT Doc_ID, "
+                                    + "Doc_header_ID, "
+                                    + "Doc_name, "
+                                    + "Date_created, "
+                                    + "Date_modified, "
+                                    + "User_ID_created, "
+                                    + "User_ID_modified, "
+                                    + "Size "
+                                    + "FROM Document_detail WHERE Doc_ID = %d",
+                            id);
+            ResultSet rs = ConnectionDB.statement.executeQuery(sql);
+            if (rs.next()) {
+                d =
+                        new DocumentDetail(
+                                rs.getInt("Doc_ID"),
+                                rs.getInt("Doc_header_ID"),
+                                rs.getString("Doc_name"),
+                                rs.getTimestamp("Date_created"),
+                                rs.getTimestamp("Date_modified"),
+                                rs.getInt("User_ID_created"),
+                                rs.getInt("User_ID_modified"),
+                                rs.getLong("Size"));
+                rs.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return d;
+    }
+
+    private static User getCurrentUser() {
+        return currentUser;
+    }
+
+    private DocumentHeader getCurrentHeader() {
+        return currentHeader;
+    }
+
+    public void setCurrentHeader(DocumentHeader currentHeader) {
+        DocumentManager.currentHeader = currentHeader;
+    }
+
+    private int getLatestID() {
+        int currentYear = Time.getCurrentBEYear();
+        int LatestID;
+        try {
+            String sql = "SELECT MAX(ID) FROM Event_log";
+            ResultSet rs = ConnectionDB.statement.executeQuery(sql);
+            if (rs.next()) {
+                LatestID = rs.getInt(1);
+                if (currentYear == LatestID / 10000) {
+                    return LatestID;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return currentYear * 10000;
+    }
+
+    public DocumentHeader createHeader(String subject) {
+        return createHeader(subject, null);
+    }
+
+    public DocumentHeader createHeader(String subject, String description) {
+        try {
+            // Add file to DocumentHeader
+            int currentID = getLatestID() + 1;
+            String t = Time.currentTimetoString();
+            String sql =
+                    String.format(
+                            "INSERT INTO Document_header "
+                                    + "VALUES(%d, '%s', %d, %d, '%s', '%s', '%s', '%s')",
+                            currentID,
+                            subject,
+                            currentUser.getUser_ID(),
+                            currentUser.getUser_ID(),
+                            t,
+                            t,
+                            description,
+                            null);
+            ConnectionDB.statement.executeUpdate(sql);
+
+            // Add log
+            Log.addLog(t, "Header: " + subject, "added", currentUser.getUsername(), currentID);
+
+            return getHeader(currentID);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static DocumentHeader getHeader(int id) {
+        DocumentHeader h = null;
+        try {
+            String sql =
+                    String.format("SELECT * FROM Document_header WHERE Doc_header_ID = %d", id);
+            ResultSet rs = ConnectionDB.statement.executeQuery(sql);
+            if (rs.next()) {
+                h =
+                        new DocumentHeader(
+                                rs.getInt("Doc_header_ID"),
+                                rs.getString("Doc_header_subject"),
+                                rs.getInt("User_ID_created"),
+                                rs.getInt("User_ID_modified"),
+                                rs.getTimestamp("Date_created"),
+                                rs.getTimestamp("Date_modified"),
+                                rs.getString("Doc_header_description"));
+                rs.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return h;
+    }
+
+    public DocumentDetail createFile(String FilePath) {
+        File f = new File(FilePath);
+        if (f.exists()) {
+            return insetFile(f);
+        }
+        System.err.println("File doesn't exits.");
+        return null;
+    }
+
+    private DocumentDetail insetFile(File file) {
+        PreparedStatement pstmt;
+        try {
+            // Add Data of file to DataFile.
+            String sql =
+                    "INSERT INTO Document_detail "
+                            + "(Doc_header_ID, "
+                            + "Doc_name, "
+                            + "Date_created, "
+                            + "Date_modified, "
+                            + "User_ID_created, "
+                            + "User_ID_modified, "
+                            + "Size, "
+                            + "Data_file) "
+                            + "VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+            pstmt = ConnectionDB.connect.prepareStatement(sql);
+            Date d = Time.getCurrentTime();
+            String t = Time.datetoString(d);
+            pstmt.setInt(1, getCurrentHeader().getDoc_header_ID());
+            pstmt.setString(2, file.getName());
+            pstmt.setString(3, t);
+            pstmt.setString(4, t);
+            pstmt.setInt(5, getCurrentUser().getUser_ID());
+            pstmt.setInt(6, getCurrentUser().getUser_ID());
+            pstmt.setLong(7, file.length());
+
+            // Add log
+            Log.addLog(
+                    Time.currentTimetoString(),
+                    file.getName(),
+                    "start uploading",
+                    getCurrentUser().getUsername());
+
+            pstmt.setBinaryStream(8, new FileInputStream(file));
+            pstmt.executeUpdate();
+            pstmt.close();
+            // Add log
+            Log.addLog(
+                    Time.currentTimetoString(),
+                    file.getName(),
+                    "uploaded successfully",
+                    getCurrentUser().getUsername());
+
+            System.out.println(String.format("Added %s Correctly.", file.getName()));
+
+            // return DocDeatail
+            sql = "SELECT MAX(Doc_ID) FROM Document_detail";
+            ResultSet rs = ConnectionDB.statement.executeQuery(sql);
+            int id = 0;
+            if (rs.next()) {
+                id = rs.getInt(1);
+            }
+
+            rs.close();
+
+            // Update Header
+            this.updateHeaderModified(d);
+
+            // Add log
+            // Log.addLog(t, file.getName(), "added", getCurrentUser().getUsername());
+
+            return getFile(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void downloadFile(int id, String targetPath) {
+        byte[] fileBytes;
+        String query;
+        try {
+            query =
+                    String.format(
+                            "SELECT Doc_name, Data_file FROM Document_detail WHERE Doc_ID = %d",
+                            id);
+            ResultSet rs = ConnectionDB.statement.executeQuery(query);
+            if (rs.next()) {
+                String name = rs.getString("Doc_name");
+                //Add Log
+                System.out.println("start downloading");
+                Log.addLog(
+                        Time.currentTimetoString(),
+                        name,
+                        "start downloading",
+                        DocumentManager.currentUser.getUsername());
+
+                fileBytes = rs.getBytes("Data_file");
+                rs.close();
+
+                //String target = new File(targetPath, id + name).getPath();
+                File target = new File(targetPath, id + name);
+                OutputStream targetFile = new FileOutputStream(target.getPath());
+
+                targetFile.write(fileBytes);
+                targetFile.close();
+                if (target.exists()) {
+                    System.out.println("downloaded successfully");
+                    Log.addLog(
+                            Time.currentTimetoString(),
+                            name,
+                            "downloaded successfully",
+                            DocumentManager.currentUser.getUsername());
+                } else {
+                    System.err.println("Download Fail ;(");
+                    Log.addLog(
+                            Time.currentTimetoString(),
+                            name,
+                            "download failed",
+                            DocumentManager.currentUser.getUsername());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateHeaderModified(Date currentTime) {
+        this.getCurrentHeader().setDate_modified(currentTime);
+        this.getCurrentHeader().setUser_ID_modified(DocumentManager.getCurrentUser().getUser_ID());
+        try {
+            String sql =
+                    String.format(
+                            "UPDATE Document_header "
+                                    + "SET User_ID_modified = '%s', "
+                                    + "Date_modified = '%s' "
+                                    + "WHERE Doc_header_ID = %d",
+                            DocumentManager.getCurrentUser().getUser_ID(),
+                            Time.datetoString(currentTime),
+                            this.getCurrentHeader().getDoc_header_ID());
+            ConnectionDB.statement.executeUpdate(sql);
+            System.out.println("Header Data was updated. :)");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
